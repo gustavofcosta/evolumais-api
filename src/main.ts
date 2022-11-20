4790435e0c6e00b674ec2bf70cc2ad463e7c9fa7
gustavofcosta <gfcdesenvolvimento@gmail.com>
2022-11-20 20:17:09 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.URL || process.env.PORT;
+  const port = 3000 || process.env.PORT;
 
   app.enableCors();
   await app.listen(port);
